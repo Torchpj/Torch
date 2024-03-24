631bf120e8d9a2e4d2ae0ee0e41925b66f3fbeb2
2is0 <130418736+Leeyoungr@users.noreply.github.com>
2024-03-24 16:42:50 +0900

    feat: onclick event setting

--- torch/src/pages/Todo_screen/Todo_screen.js
+++ torch/src/pages/Todo_screen/Todo_screen.js
@@ -3,6 +3,7 @@ import TodoList from "./Todolist";
 import AddTodo from "./Addtodo";
 import "./Todo_screen.css";
 import { GrPowerCycle } from "react-icons/gr";
+import {useHistory} from "react-router-dom"
 
 function TodoScreen() {
   const [todos, setTodos] = useState([]);
@@ -11,11 +12,16 @@ function TodoScreen() {
     setTodos([...todos, todo]);
   };
 
+  const history =useHistory();
+  const toCalendar = () => {
+    history.push('/calendar');
+  }
+
   return (
     <div className="container">
       <div className="titlecontainer">
         <h1 className="title">To-do List</h1>
-        <button className="titlebutton">
+        <button className="titlebutton" onClick={toCalendar}>
           <GrPowerCycle style={{ color: "#61ecff" }} />
         </button>
       </div>
