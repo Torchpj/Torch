92df2bf6910f8933593e2a898f91998ab01693c2
2is0 <130418736+Leeyoungr@users.noreply.github.com>
2024-03-24 15:28:56 +0900

    feat : conversion page splash to todo

--- torch/src/pages/Splash_screen/Splash_screen.js
+++ torch/src/pages/Splash_screen/Splash_screen.js
@@ -1,11 +1,23 @@
-import { useNavigate } from "react-router-dom";
+import React, {useEffect} from "react";
 import { Torch , Light} from "../../assets";
 import "./Splash_screen.css"
+import {useHistory} from "react-router-dom"
 
-export default function TorchAni() {
+export default function Splash_screen() {
 
-    const navigate=useNavigate();
+    const history =useHistory();
 
+    useEffect(() => {
+        // 5초 후 todo_screen page 이동
+        const timer = setTimeout(() => {
+            history.push('/todo');
+        }, 5000);
+    
+        // 컴포넌트 unmount: 타이머 해제
+        return () => {
+          clearTimeout(timer);
+        };
+      }, [history]); 
 
     return (
         <>
